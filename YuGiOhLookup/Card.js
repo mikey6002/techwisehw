b4af5f8cd01f286926effd1bf5afa8afbfaaa429
mikey6002 <tun32913@temple.edu>
2025-06-29 22:40:08 -0400

    Yu Gi Oh search card

--- YuGiOhLookup/Card.js
+++ YuGiOhLookup/Card.js
@@ -1,3 +1,5 @@
+
+// Yu-Gi-Oh! Card Lookup using YGOPRODeck API
 async function getCardInfo(cardName) {
   const url = `https://db.ygoprodeck.com/api/v7/cardinfo.php?name=${encodeURIComponent(cardName)}`;
   const res = await fetch(url);
@@ -6,13 +8,14 @@ async function getCardInfo(cardName) {
   if (data.error) throw new Error(data.error);
   return data.data; 
 }
-
+// Example usage
 getCardInfo("Dark Magician")
   .then(cards => {
     console.log(cards[0].name, cards[0].type, cards[0].atk, cards[0].def);
   })
   .catch(err => console.error(err));
 
+// Function to get a random Yu-Gi-Oh! card
 async function getRandomCard() {
   const res = await fetch("https://db.ygoprodeck.com/api/v7/randomcard.php");
   return res.json();
